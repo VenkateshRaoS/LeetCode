e929102c1195be99f391ab4c5b3fecaa5acdc6cf
Venkatesh Rao s <venkateshraos177@gmail.com>
2026-01-30 11:10:37 +0530

    Time: 0 ms (100%), Space: 12.4 MB (87.77%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -4,23 +4,23 @@ class Solution:
             return []
 
         phone = {
-            '2': "abc",
-            '3': "def",
-            '4': "ghi",
-            '5': "jkl",
-            '6': "mno",
-            '7': "pqrs",
-            '8': "tuv",
-            '9': "wxyz"
-        }
+            '2' : "abc",
+            '3' : "def",
+            '4' : "ghi",
+            '5' : "jkl",
+            '6' : "mno",
+            '7' : "pqrs",
+            '8' : "tuv",
+            '9' : "wxyz",
+        }        
 
-        result = [""]
+        result =[""]
 
         for digit in digits:
-            temp = []
+            temp=[]
             for comb in result:
                 for letter in phone[digit]:
                     temp.append(comb + letter)
-            result = temp
+            result=temp
 
         return result
